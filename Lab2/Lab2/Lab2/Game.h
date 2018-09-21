8fd135a210ffae6beb1f68f6785beed13a5d3cd4
James Condon <c00207200@itcarlow.ie>
2018-09-21 10:59:12 +0100

    Lab2a complete

---
+++ Lab2/Lab2/Lab2/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Enemy.h"
+#include "Player.h"
+#include <SFML/Graphics.hpp>
+
+/// <summary>
+/// @brief Main class for the SFML Playground project.
+/// 
+/// This will be a single class framework for learning about SFML.
+/// </summary>
+class Game
+{
+public:
+	Game();
+	void run();
+protected:
+	void update(double dt);
+	void render();
+	void processEvents();
+	void processGameEvents(sf::Event&);
+
+	// main window
+	sf::RenderWindow m_window;
+	Player* m_player;
+	Enemy* m_enemy;
+};
